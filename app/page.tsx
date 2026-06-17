9c771e9faa20465260cb5cde8d51bb6c51ea2756
Juan Dias <fdias.juan@gmail.com>
2026-06-17 15:25:49 -0300

    style: ajustar layout final da aplicação

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Products from "./components/Products";
 export default async function Home() {
 
   return (
-    <div className="flex justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] w-full">
+    <div className="flex justify-center min-h-screen bg-gray-100 p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] w-full">
       <Products />
     </div>
   );
